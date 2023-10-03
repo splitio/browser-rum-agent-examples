7d5ba85b52f0f9a22df8c2c26735b7a499a93951
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-10-03 11:20:00 -0300

    remove unused code: data-router, local ttest, staging URLs, etc

--- advanced/multipage-testing-example/client/index-sync.js
+++ advanced/multipage-testing-example/client/index-sync.js
@@ -2,13 +2,7 @@
 import { SplitRumAgent, webVitals } from '@splitsoftware/browser-rum-agent';
 
 SplitRumAgent
-  .setup(process.env.CLIENT_SIDE_SDK_KEY,
-    process.env.EVENTS_URL ?
-      { url: process.env.EVENTS_URL } :
-      process.env.IS_STAGING ?
-        { url: 'https://sdk.split-stage.io/api' } :
-        {}
-  )
+  .setup(process.env.CLIENT_SIDE_SDK_KEY)
   .addIdentities([
     // get key from URL query parameter `id`
     { key: new URLSearchParams(window.location.search).get('id'), trafficType: 'user' }
--- advanced/multipage-testing-example/server/index.js
+++ advanced/multipage-testing-example/server/index.js
@@ -1,10 +1,34 @@
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
-const dataRouter = require('./data');
-const appRouter = require('./app');
 
-app.use(dataRouter);
-app.use(appRouter);
+const { SplitFactory } = require('@splitsoftware/splitio');
+
+const client = SplitFactory({
+  core: {
+    authorizationKey: process.env.SERVER_SIDE_SDK_KEY,
+  },
+  debug: 'ERROR'
+}).client();
+
+// Split traffic to serve two variants of the Web page, using `id` query param as user key for feature flag evaluations
+// Web page variants are located at different folders: `dist/sync` (OFF treatment) and `dist/async` (ON treatment)
+app.use('/on', express.static(path.join(__dirname, '..', 'dist', 'async')));
+app.use('/off', express.static(path.join(__dirname, '..', 'dist', 'sync')));
+app.use('/', (req, res, next) => {
+  if (req.query.id) {
+    const treatment = client.getTreatment(req.query.id, process.env.FEATURE_FLAG_NAME);
+    console.log('serving treatment ' + treatment);
+    if (treatment === 'on') {
+      return res.redirect('/on' + req.url)
+    } else {
+      return res.redirect('/off' + req.url);
+    }
+  }
+
+  next();
+});
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');

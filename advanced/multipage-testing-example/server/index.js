fe0d31d12fdb4c411270801eab6b67eeeb58b544
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-10-03 11:44:48 -0300

    Rename files to improve code readability: Replaced 'async' with 'on' and 'sync' with 'off' for consistency.

--- advanced/multipage-testing-example/server/index.js
+++ advanced/multipage-testing-example/server/index.js
@@ -13,9 +13,9 @@ const client = SplitFactory({
 }).client();
 
 // Split traffic to serve two variants of the Web page, using `id` query param as user key for feature flag evaluations
-// Web page variants are located at different folders: `dist/sync` (OFF treatment) and `dist/async` (ON treatment)
-app.use('/on', express.static(path.join(__dirname, '..', 'dist', 'async')));
-app.use('/off', express.static(path.join(__dirname, '..', 'dist', 'sync')));
+// Web page variants are located at different folders: `dist/off` ('off' treatment) and `dist/on` ('on' treatment)
+app.use('/on', express.static(path.join(__dirname, '..', 'dist', 'on')));
+app.use('/off', express.static(path.join(__dirname, '..', 'dist', 'off')));
 app.use('/', (req, res, next) => {
   if (req.query.id) {
     const treatment = client.getTreatment(req.query.id, process.env.FEATURE_FLAG_NAME);
--- advanced/multipage-testing-example/webpack.config.js
+++ advanced/multipage-testing-example/webpack.config.js
@@ -6,37 +6,37 @@ const Dotenv = require('dotenv-webpack');
 module.exports = [{
   mode: 'production',
   entry: {
-    index: './client/index-async.js',
+    index: './client/index-on.js',
   },
   plugins: [
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Split RUM Agent example: async loading',
+      title: 'Split RUM Agent example: async loading & production mode',
       template: './client/index.html'
     }),
   ],
   output: {
     filename: '[name].bundle-on.js',
     chunkFilename: '[name].bundle-on.js',
-    path: path.resolve(__dirname, 'dist', 'async')
+    path: path.resolve(__dirname, 'dist', 'on')
   },
 }, {
   mode: 'development',
   entry: {
-    index: './client/index-sync.js',
+    index: './client/index-off.js',
   },
   plugins: [
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Split RUM Agent example: sync loading, development mode',
+      title: 'Split RUM Agent example: sync loading & development mode',
       template: './client/index.html'
     }),
   ],
   output: {
     filename: '[name].bundle-off.js',
     chunkFilename: '[name].bundle-off.js',
-    path: path.resolve(__dirname, 'dist', 'sync')
+    path: path.resolve(__dirname, 'dist', 'off')
   },
 }];
